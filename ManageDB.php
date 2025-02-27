6e0e4dc15d62cca99535cf81948fc647c38a121f
Marcos Gutiérrez <marcos.gutierrez1@alumnos.uneatlantico.es>
2025-02-27 15:36:44 +0100

    PHP funciona

--- ManageDB.php
+++ ManageDB.php
@@ -1,34 +1,45 @@
 <?php
 include_once 'DB.php';
-class ManageBD extends DB{
-  public function getQueries(){
 
-	$advisor = $this->connect()->query('SELECT * FROM advisor');
-	
-	$classroom =$this->connect()->query('SELECT * FROM classroom');
-	$course=$this->connect()->query('SELECT * FROM course');
-	$department=$this->connect()->query('SELECT * FROM department');
-	$instructor=$this->connect()->query('SELECT * FROM instructor');
-	$prereq=$this->connect()->query('SELECT * FROM prereq');
-	$section=$this->connect()->query('SELECT * FROM section');
-	$student=$this->connect()->query('SELECT * FROM student');
-	$takes=$this->connect()->query('SELECT * FROM takes');
-	$teaches=$this->connect()->query('SELECT * FROM teaches');
-	$queries = array (
-		"advisor"=>$advisor,
-		"classroom"=>$classroom,
-		"course"=>$course,
-		"department"=>$department,
-		"instructor"=>$instructor,
-		"prereq"=>$prereq,
-		"section"=>$section,
-		"student"=>$student,
-		"takes"=>$takes,
-	    "teaches"=>$teaches
-	);
-	
-		return $queries;
-	
-	}
+class ManageBD extends DB {
+    public function getQueries() {
+        $conn = $this->connect();
+
+        $course = $conn->query('SELECT * FROM course');
+        $department = $conn->query('SELECT * FROM department');
+        $instructor = $conn->query('SELECT * FROM instructor');
+        $instructor_view = $conn->query('SELECT * FROM instructor_view');
+        $profesor = $conn->query('SELECT * FROM profesor');
+        $student = $conn->query('SELECT * FROM student');
+        $takes = $conn->query('SELECT * FROM takes');
+        $teaches = $conn->query('SELECT * FROM teaches');
+
+        $students_and_professors = $conn->query('
+            SELECT id, name FROM student
+            UNION
+            SELECT id, name FROM profesor
+        ');
+
+        $professors_instructors = $conn->query('
+            SELECT p.id, p.name 
+            FROM profesor p
+            INNER JOIN instructor i ON p.id = i.id
+        ');
+
+        $queries = array(
+            "course" => $course,
+            "department" => $department,
+            "instructor" => $instructor,
+            "instructor_view" => $instructor_view,
+            "profesor" => $profesor,
+            "student" => $student,
+            "takes" => $takes,
+            "teaches" => $teaches,
+            "students_and_professors" => $students_and_professors,  // Unión
+            "professors_instructors" => $professors_instructors  // Intersección
+        );
+
+        return $queries;
+    }
 }
 ?>
